250fefe90d6616c9642707a2e16df7267ad3b5ff
nicolas cristaldo <crist4545@gmail.com>
2024-10-23 15:31:49 -0300

    commit 1

---
+++ app/src/main/java/com/nicolascristaldo/applicationviewsexample/firstapp/FirstAppActivity.kt
@@ -0,0 +1,38 @@
+package com.nicolascristaldo.applicationviewsexample.firstapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nicolascristaldo.applicationviewsexample.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btsStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+
+        btsStart.setOnClickListener {
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME",name)
+                startActivity(intent)
+            }
+        }
+    }
+}
